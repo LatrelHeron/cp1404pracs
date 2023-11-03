11e5e6a0963612156e4fb9a35a79bba7738e6191
latrelheron <latrel.heron@my.jcu.edu.au>
2023-11-03 23:49:14 +1000

    Complete guitar module for Prac 7

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -5,7 +5,15 @@ from prac_07.guitar import Guitar
 
 
 def main():
-    """Read file of guitars, store them in list and sort by year"""
+    """
+    Read file of guitars, store them in list and sort by year
+    Program to get users new guitars and store in guitars.csv
+    """
+    guitars = load_guitar()
+    get_guitar(guitars)
+
+
+def load_guitar():
     guitars = []
     in_file = open('guitars.csv', 'r')
     in_file.readline()
@@ -17,6 +25,25 @@ def main():
     guitars.sort()
     for guitar in guitars:
         print(guitar)
+    return guitars
+
+
+def get_guitar(guitars):
+    name = input("Name of guitar: ").capitalize()
+    while name != "":
+        year = int(input("Year guitar was made: "))
+        cost = float(input("Cost of guitar in $: "))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        name = input("Name of guitar: ").capitalize()
+    save_guitars(guitars)
+
+
+def save_guitars(guitars):
+    out_file = open('guitars.csv', 'w')
+    for guitar in guitars:
+        out_file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
+    out_file.close()
 
 
 main()
