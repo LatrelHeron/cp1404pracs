87d6dab391ad0116f0f25ab61b167fe91960f00f
latrelheron <latrel.heron@my.jcu.edu.au>
2023-11-05 11:32:35 +1000

    Project management outline of program and initialising class

---
+++ prac_07/project.py
@@ -0,0 +1,9 @@
+class Project:
+    def __init__(self, name="", start_date=0, priority=0, cost_estimate=0.0, percentage=0):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.percentage = percentage
+
+
